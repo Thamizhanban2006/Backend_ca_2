6e7c525a94eedf036408f33e1f20764235cb5496
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-24 14:14:19 +0530

    Completed

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Hello');
+})
+
+
+app.post('/',(req,res)=>{
+    const {email,password} = req.body;
+
+    if(!email){
+        return res.status(404).json({
+            message:"Email cannot be empty"
+        })
+    };
+
+    if(!password){
+        return res.status(404).json({
+            message:"Password cannot be empty"
+        })
+    }
+    res.status(201).json({
+        message:"Data added successfully!",
+        data:req.body
+    });
+})
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+});
